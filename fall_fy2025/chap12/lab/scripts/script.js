33ac8735381e3c81ce6af91f48c6c7877f462c4b
Aaron at GTC <ablum0568@student.gwinnetttech.edu>
2025-11-17 17:21:12 -0500

    Setup chapter 12.

---
+++ fall_fy2025/chap12/lab/scripts/script.js
@@ -0,0 +1,15 @@
+/*
+    Student Name: Aaron Blum
+    File Name: script.js
+    Date: 11/7/2025
+*/
+
+//Hamburger menu function
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
